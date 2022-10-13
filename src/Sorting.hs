fcefa4aca66cd81a28a3bc6fbbf994d0e36652c5
cd155 <github@postmail.app>
2022-10-13 15:09:15 -0400

    add sort merge and group anagrams

--- src/Sorting.hs
+++ src/Sorting.hs
@@ -1,7 +1,7 @@
 module Sorting where
 
 import Basics (Nat)
-import Data.List (sortOn)
+import Data.List (sort, sortOn, group)
 
 {-
     Bubble Sort: 
@@ -88,7 +88,7 @@ mergeSort xs = merge (mergeSort firstHalf) (mergeSort secondHalf)
 {-
     Merge two lists (A,B): 
     compare each element in A with each element in list B 
--} 
+-}
 merge :: Ord a => [a] -> [a] -> [a]
 merge [] ys = ys
 merge xs [] = xs
@@ -118,3 +118,42 @@ qsHelper p (x:xs) (left,mid,right)
     | x < p = qsHelper p xs (left++[x], mid, right)
     | x == p = qsHelper p xs (left, mid++[x], right)
     | otherwise = qsHelper p xs (left, mid, right++[x])
+
+{-
+    You are given two sorted arrays, A and B, 
+    where A has a large enough buffer at the end to hold B. 
+    Write a method to merge B into A in sorted order.
+-}
+sortedMerge :: Ord a => [a] -> [a] -> [a]
+sortedMerge [] ys = ys
+sortedMerge xs [] = xs
+sortedMerge (x:xs) (y:ys)
+    | x < y = x : sortedMerge xs (y:ys)
+    | otherwise = y : sortedMerge (x:xs) ys
+
+-- test case 
+smArrX = [1, 4, 10, 100, 1000]
+smArrY = [0, 0, 2, 1001]
+
+{-
+    Giving an array of strings, 
+    write a method to group all the anagrams
+-}
+groupAnagrams :: [String] -> [[String]]
+groupAnagrams [] = []
+groupAnagrams xs = gAHelper xs []
+
+-- loop the array to build the result
+gAHelper :: [String] -> [[String]]-> [[String]]
+gAHelper [] ys = ys
+gAHelper (x:xs) ys = gAHelper xs (gABuilder x ys)
+
+-- give a new string, add it to the result [[String]]
+gABuilder :: String -> [[String]]-> [[String]]
+gABuilder x [] = [[x]]
+gABuilder x (y:ys) 
+    | sort x == sort (head y) = (x:y):ys
+    | otherwise = [y] ++ gABuilder x ys
+
+gaArrX = ["eat","tea","tan","ate","nat","bat"]
+gaArrY = [["bat"],["nat","tan"],["ate","eat","tea"]]
