d3b98bd318c5021ab8441700d6e2d1a9837960a5
cd155 <github@postmail.app>
2022-12-08 21:24:37 -0500

    add more 8.1

--- src/Recursion.hs
+++ src/Recursion.hs
@@ -23,16 +23,14 @@ fib n = fib (n-1) + fib (n-2)
 
 -- Use memoization to store all the fib sequence in an array.
 fibMemo :: Nat -> Nat
-fibMemo 0 = 0
-fibMemo 1 = 1
 fibMemo n = fibData!!n
     where fibData = fibDataHelper 0 n [0, 1]
 
 -- Store fib sequence database from [0,1] to [0,1,...n]
 fibDataHelper :: Nat -> Nat -> [Nat] -> [Nat]
-fibDataHelper i n fibData 
-    | i+1 == n = fibData 
-    | otherwise = fibDataHelper (i+1) n 
+fibDataHelper i n fibData
+    | i+1 == n = fibData
+    | otherwise = fibDataHelper (i+1) n
         (fibData ++ [fibData!!i + fibData!!(i+1)])
 
 {-
@@ -40,7 +38,28 @@ fibDataHelper i n fibData
     A child is running up a staircase with n steps and can hop either 
     1 step, 2 steps, or 3 steps at a time. Implement a method to 
     count how many possible ways the child can run up the stairs.
+-}
+countWays :: Nat -> Nat
+countWays 0 = 1
+countWays n
+    | n < 0 = 0
+    | otherwise = countWays (n-3) + countWays (n-2) + countWays (n-1)
 
+-- Memoization version
+countWaysMemo :: Nat -> Nat
+countWaysMemo n = countWaysData!!n
+    where countWaysData = countWaysDataHelper 0 n [1,1,2]
+
+countWaysDataHelper :: Nat -> Nat-> [Nat] -> [Nat]
+countWaysDataHelper i n countData
+    | i+2 == n = countData
+    | otherwise = countWaysDataHelper (i+1) n (countData ++ 
+        [countData!!i + countData!!(i+1) + countData!!(i+2)])
+
+{-
+    8.1 extension: find all possible ways the child can run up the 
+    stairs
+    
     test case: findValidSeq 5 [1,2]
 -}
 findValidSeq :: Nat -> [Nat] -> [[Nat]]
@@ -52,7 +71,7 @@ findValidSeq stair steps = findValidSeqHelper stair steps [[]]
     update the stack with the potential sequence for the next run
 -}
 findValidSeqHelper :: Nat -> [Nat] -> [[Nat]] -> [[Nat]]
-findValidSeqHelper stair steps existSeq 
+findValidSeqHelper stair steps existSeq
     | null nextOri = ans
     | otherwise = ans ++ findValidSeqHelper stair steps nextOri
     where updateOri = permutateSeq existSeq steps
@@ -64,8 +83,8 @@ findValidSeqHelper stair steps existSeq
 {-- 
     Give a list of sequence and potential appending, and create all 
     possible permutations.
---} 
+--}
 permutateSeq :: [[Nat]] -> [Nat] -> [[Nat]]
 permutateSeq existSeq [] = []
-permutateSeq existSeq (x:xs) = map (++ [x]) existSeq ++ 
+permutateSeq existSeq (x:xs) = map (++ [x]) existSeq ++
                                permutateSeq existSeq xs
