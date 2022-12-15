6ce6b54a8ca9f65dcb1cad41098242cd35218652
cd155 <github@postmail.app>
2022-12-14 20:33:31 -0500

    add 8.2 bot move on the grid

--- src/Recursion.hs
+++ src/Recursion.hs
@@ -1,7 +1,7 @@
 module Recursion where
 
 import Basics (Nat)
-
+import Tree (findAllPaths)
 
 {-
     Recursion: the program call itself
@@ -53,7 +53,7 @@ countWaysMemo n = countWaysData!!n
 countWaysDataHelper :: Nat -> Nat-> [Nat] -> [Nat]
 countWaysDataHelper i n countData
     | i+2 == n = countData
-    | otherwise = countWaysDataHelper (i+1) n (countData ++ 
+    | otherwise = countWaysDataHelper (i+1) n (countData ++
         [countData!!i + countData!!(i+1) + countData!!(i+2)])
 
 {-
@@ -88,3 +88,82 @@ permutateSeq :: [[Nat]] -> [Nat] -> [[Nat]]
 permutateSeq existSeq [] = []
 permutateSeq existSeq (x:xs) = map (++ [x]) existSeq ++
                                permutateSeq existSeq xs
+
+{-
+    8.2
+    Imagine a robot sitting on the upper left corner of grid with r rows 
+    and c columns. The robot can only move in two directions, right and 
+    down, but certain cells are "off limits" such that the robot cannot 
+    step on them. Design an algorithm to find a path for the robot from 
+    the top left to the bottom right.
+
+    Let assume the start point is (0,0), the a normal x-axis, but a 
+    opposite y-axis (the bottom half is positive), each time we either move 
+    right or move down, this mean each time, we increase (x,y) either x or y 
+    by one until we reach to (c, r) 
+
+    Theoretically, it has 2^(c+r) different paths
+
+    test case: allPaths (0,0) (3,3) [(1,2), (3,2)]
+-}
+
+{-
+    Find the next possible move
+
+    (x, y): current position
+    (c, r): final position
+    constrs: cells are not available (can't include the start cell)
+-} 
+move :: (Nat, Nat) -> (Nat, Nat) -> [(Nat, Nat)]-> [Maybe (Nat, Nat)]
+move (x, y) (c, r) constrs
+    | x+1 <= c && y+1 <= r && xCell `notElem` constrs && yCell `notElem` constrs =
+        [Just xCell, Just yCell]
+    | x+1 <= c && y+1 <= r && xCell `elem` constrs && yCell `notElem` constrs =
+        [Nothing, Just yCell]
+    | x+1 <= c && y+1 <= r && xCell `notElem` constrs && yCell `elem` constrs =
+        [Just xCell, Nothing]
+    | x+1 <= c && y+1 <= r && xCell `elem` constrs && yCell `elem` constrs =
+        [Nothing, Nothing]
+    | x+1 > c && y+1 <= r && yCell `notElem` constrs =
+        [Nothing, Just yCell]
+    | x+1 <= c && y+1 > r && xCell `notElem` constrs =
+        [Just xCell, Nothing]
+    | otherwise = [Nothing, Nothing]
+    where xCell = (x+1, y)
+          yCell = (x, y+1)
+
+-- Check whether the cell is valid
+isValidCell :: [(Nat, Nat)] -> [(Nat, Nat)] -> Bool
+isValidCell [] _ = True
+isValidCell _ [] = True
+isValidCell (x:xs) constrs = (x `notElem` constrs) && isValidCell xs constrs
+
+-- Create a heap to record all possible sequence
+allPathsHelper ::
+    [Maybe (Nat, Nat)] -> (Nat, Nat) -> [(Nat, Nat)]-> [Maybe(Nat, Nat)]
+allPathsHelper [] _ _  = []
+allPathsHelper (Nothing:xs) final constrs
+    -- end infinite nothing loop
+    | all (== Nothing) (Nothing:xs) = []
+    | otherwise = nothingMoves ++ allPathsHelper (xs++nothingMoves) final constrs
+    where nothingMoves = [Nothing, Nothing]
+allPathsHelper (Just (x, y):xs) final constrs  =
+    nextMoves ++ allPathsHelper (xs++nextMoves) final constrs
+    where nextMoves = move (x,y) final constrs
+
+-- Return the Completed Heap
+allPaths :: (Nat, Nat) -> (Nat, Nat) -> [(Nat, Nat)] -> [Maybe(Nat, Nat)]
+allPaths start end constrs = 
+            Just start: removeNothing (allPathsHelper [Just start] end constrs)
+
+-- Return all unique paths index in a Heap
+allPaths' :: (Nat, Nat) -> (Nat, Nat) -> [(Nat, Nat)] -> [[Nat]]
+allPaths' start end constrs = findAllPaths completedHeap [] (length completedHeap-1)
+    where completedHeap = 
+            Just start: removeNothing (allPathsHelper [Just start] end constrs)
+
+-- Remove Nothing on the tail
+removeNothing :: [Maybe(Nat, Nat)] -> [Maybe(Nat, Nat)]
+removeNothing xs
+    | xs!!(length xs -1) == Nothing = removeNothing $ init xs
+    | otherwise = xs
