39c39c20febe4ab8f2ffd0111ccffdcc74b8c346
cd155 <github@postmail.app>
2022-12-25 11:34:10 -0500

    add OO parts

---
+++ src/OODesign/CallCenter/Employee.java
@@ -0,0 +1,67 @@
+package CallCenter;
+
+// 7.2
+// Imagine you have a call center with three levels of employees: 
+// respondent, manager, and director. An incoming telephone call must be 
+// first allocated to a respondent who is free. If the respondent can't 
+// handle the call, he or she must escalate the call to a manager. If the 
+// manager is not free or not able to handle it, then the call should be 
+// escalated to a director. Design the classes and data structures for 
+// this problem. Implement a method dispatchCall() which assigns a call 
+// to the first available employee.
+
+// class employee: 
+//     property:
+//         levelType
+//         isAvailable
+//         nextLevel
+//         callNumber
+    
+//     function:
+//         signCall()
+//         completeCall()
+
+
+// static method:
+//     dispatchCall([respondent], call)
+//         1. find the first available respondent, sign a call
+//             or 2. find the first available manager, sign a call
+//                 or 3. find the first available director, sign a call
+    
+//     getFirstResp([respondent]) -> a list of employee
+
+//     getFirstManager([respondent]) -> a list of employee
+
+//     getFirstDirector([respondent]) -> a list of employee
+
+//     check Resp, manage, director, to find the first one
+
+public class Employee
+{
+    private String levelType;
+    private Boolean isAvailable;
+    private Employee supervisor;
+    private String callNumber;
+
+    public String name;
+    
+    public Employee()
+    {
+        levelType = "";
+        isAvailable = false;
+        supervisor = null;
+        callNumber = "";
+    }
+
+    public void signedCall(String callNumber)
+    {
+        this.callNumber = callNumber;
+        isAvailable = false;
+    }
+
+    public void completeCall()
+    {
+        this.callNumber = null;
+        isAvailable = false;
+    }
+}
---
+++ src/OODesign/CallCenter/callFunc.java
@@ -0,0 +1,29 @@
+package CallCenter;
+import java.util.*;
+
+public class callFunc {
+    public static void dispatchCall(List<Employee> respondents)
+    {
+
+    };
+
+    private static Employee[] getFirstResp(List<Employee> respondents)
+    {
+        List<Employee> potEmp = new ArrayList<Employee>();	
+        for (Employee emp: respondents)
+        {
+            
+        }
+        return new Employee[0];
+    };
+
+    private static Employee[] getFirstManager(List<Employee> respondents)
+    {
+        return new Employee[0];
+    };
+
+    private static Employee[] getFirstDirector(List<Employee> respondents)
+    {
+        return new Employee[0];
+    };
+}
